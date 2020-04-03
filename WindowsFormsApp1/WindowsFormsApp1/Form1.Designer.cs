4ca791e594f2b33c2e1eef07d9b767c4a3961de6
ryohei.yamagishi <ryohei.yamagishi@CARERITZ4426>
2020-04-03 14:28:50 +0900

    ラベル(Hello!world!)追加

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.htmlLabel1 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.SuspendLayout();
+            // 
+            // htmlLabel1
+            // 
+            this.htmlLabel1.AutoScroll = true;
+            this.htmlLabel1.AutoScrollMinSize = new System.Drawing.Size(82, 25);
+            this.htmlLabel1.AutoSize = false;
+            this.htmlLabel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.htmlLabel1.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.htmlLabel1.Font = new System.Drawing.Font("游ゴシック", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.htmlLabel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.htmlLabel1.Location = new System.Drawing.Point(56, 30);
+            this.htmlLabel1.Name = "htmlLabel1";
+            this.htmlLabel1.Size = new System.Drawing.Size(82, 25);
+            this.htmlLabel1.TabIndex = 0;
+            this.htmlLabel1.Text = "Hello!world!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.htmlLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1;
+    }
+}
+
